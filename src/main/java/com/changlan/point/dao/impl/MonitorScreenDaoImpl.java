55e916e5171966cb79d2270c66b531e36ea339a6
1445023633 <1445023633@qq.com>
2019-07-15 16:24:22 +0800

    增加线路 id 查询

--- src/main/java/com/changlan/point/dao/impl/MonitorScreenDaoImpl.java
+++ src/main/java/com/changlan/point/dao/impl/MonitorScreenDaoImpl.java
@@ -54,7 +54,7 @@ public class MonitorScreenDaoImpl implements IMonitorScreenDao{
 		em.clear();
 		String sql =" select t.`STATUS` , t.POINT_ID , t.POINT_NAME ,  t.LONG_LATI, t.LINE_ID,  t.LINE_ORDER ,(select  s.LINE_NAME  from tbl_lines s  where t.LINE_ID = s.LINE_ID) as LINE_NAME from tbl_points t, tbl_lines k  where k.LINE_ID = t.LINE_ID   ";
 		if(search != null) {
-			sql += " and ( t.POINT_NAME like " +  "'%" + search + "%'" +  " or k.LINE_NAME like " +  "'%" + search + "%'" + ")";
+			sql += " and ( t.POINT_NAME like " +  "'%" + search + "%'" +  " or k.LINE_NAME like " +  "'%" + search + "%'" +  " or t.LINE_ID like " +  "'%" + search + "%'" + ")";
 		}
 		
 		Query createNativeQuery = em.createNativeQuery(SqlUtil.addRowId(sql.toString()));
