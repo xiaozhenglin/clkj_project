f00d13b7a22537db2e2c735e8a5eef46ed4cf720
1445023633 <1445023633@qq.com>
2019-07-15 15:48:30 +0800

    长时间验证码验证

--- src/main/java/com/changlan/user/action/LoginController.java
+++ src/main/java/com/changlan/user/action/LoginController.java
@@ -79,7 +79,7 @@ public class LoginController extends BaseController{
 			return success(UserErrorType.VERIFIED_NULL.getCode(),UserErrorType.VERIFIED_NULL.getMsg(),false,null); 
 		}
 		//不是生成的验证码报 验证失败错误
-		if(!(verifyCode.trim()).equalsIgnoreCase((generalCode.trim()))){
+		if(!(verifyCode.trim()).equalsIgnoreCase((generalCode))){
 			return success(UserErrorType.VERIFIED_ERROR.getCode(),UserErrorType.VERIFIED_ERROR.getMsg(),false,null); 
 		}
 		LocalDateTime time = LocalDateTime.now();
