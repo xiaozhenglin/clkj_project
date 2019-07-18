75a887d5f356951e80f17bc065af7e41cdac829a
1445023633 <1445023633@qq.com>
2019-07-18 16:33:24 +0800

    qudiao

--- src/main/java/com/changlan/common/action/ErrorController.java
+++ src/main/java/com/changlan/common/action/ErrorController.java
@@ -1,24 +1,24 @@
-package com.changlan.common.action;
-
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class ErrorController {
-
-    @Resource
-    private HttpServletRequest request;
-
-    /**
-     * 重新抛出异常
-     * @throws Exception 
-     */
-    @RequestMapping("/error/rethrow")
-    public void rethrow() throws Exception { 
-        throw  (Exception)request.getAttribute("error");
-    }
-}
-
+//package com.changlan.common.action;
+//
+//import javax.annotation.Resource;
+//import javax.servlet.http.HttpServletRequest;
+//
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class ErrorController {
+//
+//    @Resource
+//    private HttpServletRequest request;
+//
+//    /**
+//     * 重新抛出异常
+//     * @throws Exception 
+//     */
+//    @RequestMapping("/error/rethrow")
+//    public void rethrow() throws Exception { 
+//        throw  (Exception)request.getAttribute("error");
+//    }
+//}
+//
