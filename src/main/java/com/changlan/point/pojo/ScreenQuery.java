69d033e136b3c6559accd74e99ba62e175b49d4d
1445023633 <1445023633@qq.com>
2019-07-22 10:23:41 +0800

    ScreenQuery提交

---
+++ src/main/java/com/changlan/point/pojo/ScreenQuery.java
@@ -0,0 +1,33 @@
+package com.changlan.point.pojo;
+
+public class ScreenQuery {
+	private String lineName;
+	private String pointName;
+	private Integer LineId;
+	private Integer pointId;
+	public String getLineName() {
+		return lineName;
+	}
+	public void setLineName(String lineName) {
+		this.lineName = lineName;
+	}
+	public String getPointName() {
+		return pointName;
+	}
+	public void setPointName(String pointName) {
+		this.pointName = pointName;
+	}
+	public Integer getLineId() {
+		return LineId;
+	}
+	public void setLineId(Integer lineId) {
+		LineId = lineId;
+	}
+	public Integer getPointId() {
+		return pointId;
+	}
+	public void setPointId(Integer pointId) {
+		this.pointId = pointId;
+	}
+	
+}
